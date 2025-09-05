cf27f90069e78fc2d19e89add17731e6957f5077
Maker’s Cave <makerscave@Makerss-MacBook-Pro.local>
2025-09-04 21:51:51 -0500

    Navbar implementation

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/sidebar";
+import { Main } from "next/document";
+import Navbar from "@/components/navbar";
+import { ThemeProvider } from "@/components/providers/themeprovider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +27,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange={false}>
+          <Sidebar />
+          <main className="w-full">
+            <Navbar />
+            <div className="px-4"> {children}</div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,8 +4,8 @@ import { BadgePlus } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button variant="destructive" size="lg"><BadgePlus /> Click me2</Button>
+    <div>
+
     </div>
   );
 }
